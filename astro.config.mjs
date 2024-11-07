4a66aa67266e6faf6a89a8b968f21e1cf4c8a293
Fig3 <58235506+Fig3@users.noreply.github.com>
2024-11-07 14:03:59 +0900

    追加: 公式チュートリアル5-1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://exquisite-toffee-be1fe7.netlify.app"
+  site: "https://exquisite-toffee-be1fe7.netlify.app",
+  integrations: [preact()]
 });
